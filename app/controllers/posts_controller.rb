502eb7c8b2bbb6ab35fe3ec87c076da2df4c863a
Midi <midi13@163.com>
2016-11-30 15:31:36 +0800

    require_login only apply to update destory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_login
+  before_action :require_login, only: [:update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
